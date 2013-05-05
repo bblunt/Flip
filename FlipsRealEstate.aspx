6181d82b6919bc72c79923fb90deddb587b05e39
Chris H. <chavens@ksu.edu>
2013-05-05 00:01:45 -0500

    Most of Main Page is complete

--- FlipsRealEstate.aspx
+++ FlipsRealEstate.aspx
@@ -5,14 +5,135 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Flip's Real Estate</title>
+    <style type="text/css">
+        .style1
+        {
+            width: 80%;
+        }
+        .style2
+        {
+            width: 94%;
+            color: #FFFF00;
+            margin-bottom: 1px;
+            background-color: #FFFF00;
+        }
+        .style3
+        {
+            width: 206px;
+        }
+        .style4
+        {
+            width: 210px;
+        }
+        .style5
+        {
+            background-color: #FFFF66;
+        }
+        .style6
+        {
+            color: #400040;
+            background-color: #FFFF66;
+        }
+        .style7
+        {
+            width: 237px;
+            background-color: #FFFF00;
+            text-align: center;
+        }
+        .style8
+        {
+            width: 1058px;
+            height: 295px;
+        }
+        .style9
+        {
+            width: 419px;
+            font-size: xx-large;
+            color: #660066;
+            background-color: #FFFF00;
+        }
+    </style>
 </head>
 <body>
     <form id="frmFlipsRealEstate" runat="server">
     <div>
-    
+    <center
         <asp:Image ID="imgBanner" runat="server" ImageUrl="~/Images/FlipBanner.png" />
-    
+        <img alt="" class="style8" src="Images/FlipBanner.png" /></center>
     </div>
+    <center>
+    <table align="center" class="style1">
+        <tr>
+            <td bgcolor="#ED1C24" class="style7">
+                <asp:HyperLink ID="HyperLink1" runat="server" 
+                    NavigateUrl="~/RealEstateAgents.aspx" style="font-size: xx-large">Agents</asp:HyperLink>
+            </td>
+            <td class="style7">
+                <asp:HyperLink ID="HyperLink2" runat="server" 
+                    NavigateUrl="~/RealEstateListings.aspx" style="font-size: xx-large">Listings</asp:HyperLink>
+            </td>
+            <td class="style7" width="237 px">
+                <asp:HyperLink ID="HyperLink3" runat="server" 
+                    NavigateUrl="~/RealEstateListingsByAgent.aspx" style="font-size: xx-large">Listings by Agent</asp:HyperLink>
+            </td>
+        </tr>
+    </table>
+   </center>
+        <center>
+        <center style="background-color: #ED1C24; width: 1058px; margin-top: 0px;"> 
+        
+            <br />
+            <br />
+        
+            <asp:Label ID="Label1" runat="server" 
+                Text="While you are here, look up your current weather!" CssClass="style9"></asp:Label>
+
+            <br />
+
+            <br class="style6" />
+            
+            <br class="style6" />
+            <table class="style2">
+                <tr>
+                    <td class="style3">
+                        <asp:Label ID="Label2" runat="server" Text="Country" CssClass="style6"></asp:Label>
+                    </td>
+                    <td class="style9">
+                        <asp:DropDownList ID="CountryDropDownList" runat="server" CssClass="style5">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style3">
+                        <asp:Label ID="Label3" runat="server" Text="City" CssClass="style6"></asp:Label>
+                    </td>
+                    <td class="style9">
+                        <asp:DropDownList ID="CityDropDownList" runat="server" style="margin-left: 0px" 
+                            CssClass="style5">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+            </table>
+            <br class="style6" />
+            <br class="style6" />
+            <table class="style2">
+                <tr>
+                    <td class="style4">
+                        <asp:Label ID="Label4" runat="server" Text="Info" CssClass="style6"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="WeatherLabel" runat="server" CssClass="style6"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br class="style6" />
+            <asp:Button ID="NewWeatherButton" runat="server" Text="New Weather" 
+                Height="26px" CssClass="style5" />
+        </center>
+        </center>
+    <p>
+        &nbsp;</p>
+   
     </form>
-</body>
+    </body>
 </html>
--- FlipsRealEstate.aspx.vb
+++ FlipsRealEstate.aspx.vb
@@ -1,4 +1,111 @@
+Imports System.Xml
+Imports net.webservicex.www
+
 Partial Class FlipRealEstate
     Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Me.IsPostBack Then
+            'Create a new global weather object
+            Dim proxy As New GlobalWeather
+            'Create a new xml document object. We need xml implementation since the data is in xml format.
+            Dim xmlDoc As New XmlDocument
+            'Load all country/city names in to the xml document.
+            xmlDoc.LoadXml(proxy.GetCitiesByCountry(""))
+            'Query only country names and put them in to xml node list
+            Dim xmlNodes As XmlNodeList = xmlDoc.GetElementsByTagName("Country")
+            Dim countryList As New List(Of String)
+            For Each node As XmlNode In xmlNodes
+                If Not countryList.Contains(node.InnerText) Then
+                    'Loop through the country names and put them in to a list object
+                    countryList.Add(node.InnerText)
+                End If
+            Next
+            countryList.Sort()
+            'Bind the CountryDropDownList control.
+            Me.CountryDropDownList.DataSource = countryList
+            Me.CountryDropDownList.DataBind()
+            Me.CountryDropDownList_SelectedIndexChanged(Me.CountryDropDownList, New EventArgs)
+        End If
+    End Sub
+    Protected Sub CountryDropDownList_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles CountryDropDownList.SelectedIndexChanged
+        If Me.CountryDropDownList.SelectedItem IsNot Nothing Then
+            Dim cityList As New List(Of String)
+            Dim proxy As New GlobalWeather
+            Dim xmlDoc As New XmlDocument
+            'Load the xml document with all city names for the selected country.
+            xmlDoc.LoadXml(proxy.GetCitiesByCountry(Me.CountryDropDownList.SelectedValue))
+            'Get all city names and put them in to xml node list.
+            Dim xmlNodes As XmlNodeList = xmlDoc.GetElementsByTagName("City")
+            For Each node As XmlNode In xmlNodes
+                If Not cityList.Contains(node.InnerText) Then
+                    'Loop through the city names and put them in to a list object
+                    cityList.Add(node.InnerText)
+                End If
+            Next
+            cityList.Sort()
+            'Bind the CityDropDownList control.
+            Me.CityDropDownList.DataSource = cityList
+            Me.CityDropDownList.DataBind()
+        Else
+            Me.CityDropDownList.DataSource = Nothing
+            Me.CityDropDownList.DataBind()
+        End If
+    End Sub
+    Protected Sub Page_LoadComplete(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.LoadComplete
+        If Me.CountryDropDownList.SelectedItem IsNot Nothing _
+                AndAlso Me.CityDropDownList.SelectedItem IsNot Nothing Then
+            Dim sb As New StringBuilder
+            Dim proxy As New GlobalWeather
+            'Get weather information for the selected country and city by using the "GetWeather" service method.
+            Dim xmlStr As String = proxy.GetWeather(Me.CityDropDownList.SelectedValue, Me.CountryDropDownList.SelectedValue)
+            Dim success As Boolean = False
+            Try
+                Dim xmlDoc As New XmlDocument
+                'Load the xml string data into xml document
+                xmlDoc.LoadXml(xmlStr)
+                Dim xmlNode As XmlNode = xmlDoc.DocumentElement()
+                For Each node As XmlNode In xmlNode.ChildNodes
+                    If node.Name = "Status" Then
+                        success = node.InnerText = "Success"
+                    Else
+                        'Loop through the nodes and put the information in the StringBuilder object.
+                        sb.Append("<b>" & node.Name & ":</b> " & node.InnerText & "<br />")
+                    End If
+                Next
+            Catch ex As Exception
+                sb.Append("Data Not Found")
+            End Try
+            If success Then
+                'If everything went well than show the information.
+                Me.WeatherLabel.Text = sb.ToString
+            Else
+                Me.WeatherLabel.Text = "Data Not Found"
+            End If
+        End If
 
+    End Sub
+    Protected Sub NewWeatherButton_Click(sender As Object, e As System.EventArgs)
+        If Not Me.IsPostBack Then
+            'Create a new global weather object
+            Dim proxy As New GlobalWeather
+            'Create a new xml document object. We need xml implementation since the data is in xml format.
+            Dim xmlDoc As New XmlDocument
+            'Load all country/city names in to the xml document.
+            xmlDoc.LoadXml(proxy.GetCitiesByCountry(""))
+            'Query only country names and put them in to xml node list
+            Dim xmlNodes As XmlNodeList = xmlDoc.GetElementsByTagName("Country")
+            Dim countryList As New List(Of String)
+            For Each node As XmlNode In xmlNodes
+                If Not countryList.Contains(node.InnerText) Then
+                    'Loop through the country names and put them in to a list object
+                    countryList.Add(node.InnerText)
+                End If
+            Next
+            countryList.Sort()
+            'Bind the CountryDropDownList control.
+            Me.CountryDropDownList.DataSource = countryList
+            Me.CountryDropDownList.DataBind()
+            Me.CountryDropDownList_SelectedIndexChanged(Me.CountryDropDownList, New EventArgs)
+        End If
+    End Sub
 End Class
