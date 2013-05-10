8740c7a5b8eea724abe6613f75409e0bb8e1b3aa
Chris Havenstein <TiTlPfMANGT570>
2013-05-10 01:17:36 -0500

    Making progress on the password prompt/enforcement
    I currently have created an area on the Agents page where I have created
    a custom validation control that requires you to enter the password
    "carnie" to proceed. The "OK" button in the same area validates the
    validation group "Password"; which the custom validation control is part
    of. In theory this should make the page valid.  The Custom Validation
    control works well with the password. I just can't get the Page.IsValid
    property to enforce conditionally (through if statements) the
    "dvAgents_ItemInserting", "gvAgents_RowDeleting", "gvAgents_RowUpdating"
    events properly.

--- RealEstateAgents.aspx.vb
+++ RealEstateAgents.aspx.vb
@@ -7,8 +7,10 @@ Partial Class RealEstateAgents
     End Sub
 
     Protected Sub btnInsertAgent_Click(sender As Object, e As EventArgs) Handles btnInsertAgent.Click
-        Me.btnInsertAgent.Visible = False
-        Me.dvAgents.Visible = True
+        If Page.IsValid Then
+            Me.btnInsertAgent.Visible = False
+            Me.dvAgents.Visible = True
+        End If
     End Sub
 
     Protected Sub CancelAgentInsert(sender As Object, e As EventArgs)
@@ -17,19 +19,25 @@ Partial Class RealEstateAgents
     End Sub
 
     Protected Sub dvAgents_ItemInserting(sender As Object, e As DetailsViewInsertEventArgs) Handles dvAgents.ItemInserting
-        Me.dsAgents.InsertParameters.Item("@AgentID").DefaultValue = Me.gvAgents.DataKeys.Count + 1
-        Me.dsAgents.InsertParameters.Item("@AgentPic").DefaultValue = _
-            CType(Me.dvAgents.FindControl("txtAgentPic"), WebControls.TextBox).Text
-        Me.dsAgents.InsertParameters.Item("@First_Name").DefaultValue = _
-        CType(Me.dvAgents.FindControl("txtFirstName"), WebControls.TextBox).Text
-        Me.dsAgents.InsertParameters.Item("@Last_Name").DefaultValue = _
-            CType(Me.dvAgents.FindControl("txtLastName"), WebControls.TextBox).Text
-        Me.dsAgents.InsertParameters.Item("@Address").DefaultValue = _
-            CType(Me.dvAgents.FindControl("txtAddress"), WebControls.TextBox).Text
-        Me.dsAgents.InsertParameters.Item("@City").DefaultValue = _
-            CType(Me.dvAgents.FindControl("txtCity"), WebControls.TextBox).Text
-        Me.dsAgents.InsertParameters.Item("@Phone_Number").DefaultValue = _
-            CType(Me.dvAgents.FindControl("txtPhoneNumber"), WebControls.TextBox).Text
+
+        If Page.IsValid Then
+            tbPassword.Visible = False
+            lblPassword.Visible = False
+            btnOkPassword.Visible = False
+            Me.dsAgents.InsertParameters.Item("@AgentID").DefaultValue = Me.gvAgents.DataKeys.Count + 1
+            Me.dsAgents.InsertParameters.Item("@AgentPic").DefaultValue = _
+                CType(Me.dvAgents.FindControl("txtAgentPic"), WebControls.TextBox).Text
+            Me.dsAgents.InsertParameters.Item("@First_Name").DefaultValue = _
+            CType(Me.dvAgents.FindControl("txtFirstName"), WebControls.TextBox).Text
+            Me.dsAgents.InsertParameters.Item("@Last_Name").DefaultValue = _
+                CType(Me.dvAgents.FindControl("txtLastName"), WebControls.TextBox).Text
+            Me.dsAgents.InsertParameters.Item("@Address").DefaultValue = _
+                CType(Me.dvAgents.FindControl("txtAddress"), WebControls.TextBox).Text
+            Me.dsAgents.InsertParameters.Item("@City").DefaultValue = _
+                CType(Me.dvAgents.FindControl("txtCity"), WebControls.TextBox).Text
+            Me.dsAgents.InsertParameters.Item("@Phone_Number").DefaultValue = _
+                CType(Me.dvAgents.FindControl("txtPhoneNumber"), WebControls.TextBox).Text
+        End If
     End Sub
 
     Protected Sub dvAgents_ItemInserted(sender As Object, e As DetailsViewInsertedEventArgs) Handles dvAgents.ItemInserted
@@ -37,24 +45,74 @@ Partial Class RealEstateAgents
         Me.btnInsertAgent.Visible = True
         Me.dvAgents.Visible = False
     End Sub
+    ''testing this; it doesn't work yet.
 
-    Protected Sub gvAgents_RowDeleting(sender As Object, e As GridViewDeleteEventArgs) Handles gvAgents.RowDeleting
-        Me.dsAgents.DeleteParameters.Item("@AgentID").DefaultValue = Me.gvAgents.DataKeys(e.RowIndex).Value
+    '' Protected Sub gvAgents_RowDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles gvAgents.RowDataBound
+    ''     If e.Row.RowType = DataControlRowType.DataRow Then
+
+    '' Dim eb As ButtonType = CType(e.Row.FindControl("EditButton1"), ButtonType) 'Edit LinkButton
+    '' Dim ec As ButtonType = CType(e.Row.FindControl("DeleteButton1"), ButtonType) 'Delete LinkButton
+    ''      If Page.IsValid Then
+    ''           eb.Visible = True
+    ''         ec.Visible = True
+    ''       End If
+
+    ''    End If
+    '' End Sub
+
+    Protected Sub gvAgents_RowDeleting(ByVal sender As Object, ByVal e As GridViewDeleteEventArgs) Handles gvAgents.RowDeleting
+
+        Page.Validate("Password")
+        If Page.IsValid = True Then
+            Me.dsAgents.DeleteParameters.Item("@AgentID").DefaultValue = Me.gvAgents.DataKeys(e.RowIndex).Value
+        End If
     End Sub
 
-    Protected Sub gvAgents_RowUpdating(sender As Object, e As GridViewUpdateEventArgs) Handles gvAgents.RowUpdating
-        Me.dsAgents.UpdateParameters.Item("@AgentPic").DefaultValue = _
-            CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtAgentPic"), WebControls.TextBox).Text
-        Me.dsAgents.UpdateParameters.Item("@First_Name").DefaultValue = _
-            CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtFirstName"), WebControls.TextBox).Text
-        Me.dsAgents.UpdateParameters.Item("@Last_Name").DefaultValue = _
-            CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtLastName"), WebControls.TextBox).Text
-        Me.dsAgents.UpdateParameters.Item("@Address").DefaultValue = _
-            CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtAddress"), WebControls.TextBox).Text
-        Me.dsAgents.UpdateParameters.Item("@City").DefaultValue = _
-            CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtCity"), WebControls.TextBox).Text
-        Me.dsAgents.UpdateParameters.Item("@Phone_Number").DefaultValue = _
-            CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtPhoneNumber"), WebControls.TextBox).Text
-        Me.dsAgents.UpdateParameters.Item("@AgentID").DefaultValue = Me.gvAgents.DataKeys(e.RowIndex).Value
+    Protected Sub gvAgents_RowUpdating(ByVal sender As Object, ByVal e As GridViewUpdateEventArgs) Handles gvAgents.RowUpdating
+        Page.Validate("Password")
+        If Page.IsValid Then
+            tbPassword.Visible = False
+            lblPassword.Visible = False
+            btnOkPassword.Visible = False
+
+            Me.dsAgents.UpdateParameters.Item("@AgentPic").DefaultValue = _
+                CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtAgentPic"), WebControls.TextBox).Text
+            Me.dsAgents.UpdateParameters.Item("@First_Name").DefaultValue = _
+                CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtFirstName"), WebControls.TextBox).Text
+            Me.dsAgents.UpdateParameters.Item("@Last_Name").DefaultValue = _
+                CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtLastName"), WebControls.TextBox).Text
+            Me.dsAgents.UpdateParameters.Item("@Address").DefaultValue = _
+                CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtAddress"), WebControls.TextBox).Text
+            Me.dsAgents.UpdateParameters.Item("@City").DefaultValue = _
+                CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtCity"), WebControls.TextBox).Text
+            Me.dsAgents.UpdateParameters.Item("@Phone_Number").DefaultValue = _
+                CType(Me.gvAgents.Rows(e.RowIndex).FindControl("txtPhoneNumber"), WebControls.TextBox).Text
+            Me.dsAgents.UpdateParameters.Item("@AgentID").DefaultValue = Me.gvAgents.DataKeys(e.RowIndex).Value
+        End If
+    End Sub
+
+    Protected Sub btnOkPassword_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnOkPassword.Click
+        Page.Validate("Password")
+        If Page.IsValid Then
+            tbPassword.Visible = False
+            btnOkPassword.Visible = False
+            lblPassword.Visible = False
+        End If
+    End Sub
+
+    Protected Sub CustomValidator1_ServerValidate(ByVal source As Object, ByVal args As System.Web.UI.WebControls.ServerValidateEventArgs) Handles CustomValidator1.ServerValidate
+
+        If args.IsValid = (tbPassword.Text = "carnie") Then
+            args.IsValid = True
+        Else
+            args.IsValid = False
+        End If
+    End Sub
+
+    Protected Sub gvAgents_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles gvAgents.SelectedIndexChanged
+        lblPassword.Visible = True
+        tbPassword.Visible = True
+        btnOkPassword.Visible = True
+
     End Sub
 End Class
